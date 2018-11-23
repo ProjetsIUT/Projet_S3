695b1f268f16bf0281cdd8e17703828da03f1b45
Adrien Chaptal <adrien.chaptal@gmail.com>
2018-11-23 17:01:27 +0100

    ok

--- view/error.php
+++ view/error.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 
-<div class='div_error' style="background-color:red; text-align:center;">
-
-
-<a> Nous ne pouvons pas traiter votre demande</a>
-<a> Code d'erreur : <?php echo $error_code?></a>
-
-=======
 <div class='div_error'>
 
 <a> Nous ne pouvons pas traiter votre demande</a>
 <a> Code d'erreur : <?php echo $error_code?></a>
 
->>>>>>> AdrienDev
 </div>
--- view/view.php
+++ view/view.php
@@ -1,48 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-    <head>
-		<meta charset="utf-8" />
-		<title><?php echo $pagetitle?></title>
-			<link rel="stylesheet" type="text/css" href=<?php echo (File::build_path(array('style.css'))) ?>>
-	</head>
-	<body id=<?php if(isset($page_id)){ echo ("body_" . $page_id); } ?>>
-	
-		<header>
-				<div id="logo"><img src="img/LogoHomepage3.png" alt="LogoAgora"> </div>
 
-				
-				<nav>
-
-						<div class="menu">
-							Cours
-						</div>
-						<div class="menu" href="./index.php/?controller=QCM&action=show_form_new">
-							<a href=<?php echo (File::build_path(array('index.php'))) . '?controller=QCM&action=show_form_new' ?>>Exercices</a>
-						</div>
-						<div class="menu">
-							Statistiques
-						</div>
-						
-						
-				</nav>
-
-				
-				<div id="connexion" ><a href=<?php echo (File::build_path(array('index.php'))) . '?controller=Utilisateur&action=show_login_page' ?> style="text-decoration:none;" id="connexion_inscription">Connexion | Inscription</a></div>
-		</header>
-
-		<?php
-
-
-			$filepath = File::build_path(array("view", ucfirst(self::$object), "$view.php"));
-			require $filepath; 
-
-		?>
-
-
-	</body>
-</html>
-=======
 <!DOCTYPE html>
 <html>
     <head>
@@ -86,4 +42,4 @@
 
 	</body>
 </html>
->>>>>>> AdrienDev
+
