01141d282b7ed425575f415a681e5ba0766f8c92
Julian Bourdes <julian.bourdes@gmail.com>
2018-11-23 17:22:48 +0100

    Update routeur.php

--- controller/routeur.php
+++ controller/routeur.php
@@ -1,51 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-require_once (File::build_path(array('controller', 'ControllerUtilisateur.php')));
-$controller_default = 'utilisateur';
-
-if(isset($_COOKIE['preference'])) {
-	$controller_default = unserialize($_COOKIE['preference']);
-}
-
-if(!isset($_GET['controller'])) {
-	$controller = $controller_default;
-} else {
-	$controller = $_GET['controller'];
-}
-
-$controller_class = 'Controller' . ucfirst($controller);
-
-if(class_exists($controller_class)){
-	
-	if(!isset($_GET['action'])) {
-		$action = 'selectAll';
-	}
-	else {
-		$action = $_GET['action'];
-	}
-
-	if (isset($action)) {
-
-		$class_methods = get_class_methods($controller_class);
-
-		if(in_array($action, $class_methods)) {
-			$controller_class::$action();
-		}
-		else {
-			$view = 'error';
-			$pagetitle = 'Erreur';
-			require (File::build_path(array('view', 'view.php')));
-		}
-	}
-
-}
-else {
-	$view = 'error';
-	$pagetitle = 'Erreur';
-	require (File::build_path(array('view', 'view.php')));
-}
-=======
 			$controller_class="ControllerGeneral"; //Contrôleur par défaut 
 
 
@@ -82,6 +36,5 @@ else {
 
 			}
 
->>>>>>> master
 
 ?>
