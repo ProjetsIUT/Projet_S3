f19921ecdcb4dadab5d1398f8ea0c006ebf043be
Yael Misseri <yael.misseri@etu.umontpellier.fr>
2018-11-24 14:44:36 +0100

    Fin du module de connexion

--- view/view.php
+++ view/view.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html id=<?php if(isset($page_id)){ echo ("page_" . $page_id); } ?>>
+<html id=<?php if(isset($page_id)){ echo ($page_id); } ?>>
     <head>
 		<meta charset="utf-8" />
 		<title><?php echo $pagetitle?></title>
-			<link rel="stylesheet" type="text/css" href=<?php echo (File::build_path(array('style.css'))) ?>>
+			<link rel="stylesheet" type="text/css" href="./style.css">
 	
 <header>
 				<a href="./" id="logo"><img src="img/LogoHomepage3.png" alt="LogoAgora"> </a>
@@ -20,7 +20,7 @@
 				</nav>
 
 				
-				<div id="connexion" ><a href=<?php echo (File::build_path(array('index.php'))) . '?controller=Utilisateur&action=show_login_page' ?> style="text-decoration:none;" id="connexion_inscription">Connexion | Inscription</a></div>
+				<div id="connexion" ><a href=<?php echo (File::build_path(array('index.php'))) . '?controller=Utilisateurs&action=show_login_page' ?> style="text-decoration:none;" id="connexion_inscription">Connexion | Inscription</a></div>
 		</header>
 	</head>
 
