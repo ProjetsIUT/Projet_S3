0e0ed2e9ec3f46734d5482802edaaeb7017bed78
Yael Misseri <yael.misseri@etu.umontpellier.fr>
2018-12-17 13:06:41 +0100

    Merge

---
+++ controller/ControllerMatieres.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$path=array('model','ModelEnseignants.php');
+require_once File::build_path($path);
+
+$path=array('lib','Security.php');
+require_once File::build_path($path);
+
+class ControllerMatieres {
+
+	protected static $object = "matieres";
+	
+
+
+}
+
+
+?>
