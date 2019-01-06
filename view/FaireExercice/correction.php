fa4092d79d5a8844c53c5432f001cc9020f5b132
Adrien Chaptal <adrien.chaptal@gmail.com>
2019-01-06 18:22:48 +0100

    correction

--- view/FaireExercice/correction.php
+++ view/FaireExercice/correction.php
@@ -1,10 +1,11 @@
 <?php
+    echo "<div>";
     echo "<h2> $nomE</h2>  <br/ >";
     echo "<p> Énoncé de l'exercice : $enonce </p>"; 
 
-    echo "<p> Reponse de l'eleve $loginEtudiant : <br />
-              $reponse</p>"
+    echo "<p> Reponse de l'eleve $loginEtudiant : <br /> $reponse</p>";
 
+    echo "</div>";
 ?>
 
             <form method="post" action="index.php?" id="formulaire_ajouterqcm">
@@ -12,16 +13,16 @@
             <fieldset>
             
             <p>
-                Note sur 10 : <br />
-                <input type="number" name="note" max=10 min=0>
+                Note sur 20 : <br />
+                <input type="number" name="note" max=20 min=0>
             </p>
 
             <p>
                 <label for="reponse_id">Commentaires:</label>
                 <textarea name="reponse" id="reponse_id" required></textarea>
                 
-                <input type="hidden" name="action" value="reponse" />
-                <input type="hidden" name="controller" value="FaireExercice" />
+                <input type="hidden" name="action" value="" />
+                <input type="hidden" name="controller" value="Notes" />
                 <input type="hidden" name="idExercice" value=<?=$id?> />
 
             </p>
