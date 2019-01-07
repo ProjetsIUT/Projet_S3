18a6152dbbff0d15dc44f1703ea952535c3741de
Julian Bourdes <julian.bourdes@gmail.com>
2019-01-08 00:48:15 +0100

    Maj icon et logo + vue

--- view/view.php
+++ view/view.php
@@ -2,6 +2,7 @@
 
 <html id=<?php if(isset($page_id)){ echo ($page_id); } ?>>
     <head>
+		<link rel="icon" href="img/Agora_icone_finale_CR_JulieValentin.png" />
 		<meta charset="utf-8" />
 		<title><?php echo $pagetitle?></title>
 			<link rel="stylesheet" type="text/css" href="./style.css">
@@ -9,16 +10,16 @@
 <header>
 				<?php
 				if (Session::is_teacher()) {
-					echo '<a href="index.php?controller=enseignants&action=show_perso_page" id="logo"><img src="img/LogoHomepage3.png" alt="LogoAgora"> </a>';
+					echo '<a href="index.php?controller=enseignants&action=show_perso_page" id="logo"><img src="img/Agora_logo_final_CR_JulieValentin_redimensionne.png" alt="LogoAgora"> </a>';
 				}
 				else if (Session::is_student()) {
-					echo '<a href="index.php?controller=etudiants&action=show_perso_page" id="logo"><img src="img/LogoHomepage3.png" alt="LogoAgora"> </a>';
+					echo '<a href="index.php?controller=etudiants&action=show_perso_page" id="logo"><img src="img/Agora_logo_final_CR_JulieValentin_redimensionne.png" alt="LogoAgora"> </a>';
 				}
 				else if (Session::is_admin()) {
-					echo '<a href="index.php?controller=administrateur&action=show_perso_page" id="logo"><img src="img/LogoHomepage3.png" alt="LogoAgora"> </a>';
+					echo '<a href="index.php?controller=administrateur&action=show_perso_page" id="logo"><img src="img/Agora_logo_final_CR_JulieValentin_redimensionne.png" alt="LogoAgora"> </a>';
 				} 
 				else if (!isset($_SESSION['typeUtilisateur'])){
-					echo '<a href="index.php?controller=Utilisateurs&action=show_login_page" id="logo"><img src="img/LogoHomepage3.png" alt="LogoAgora"> </a>';
+					echo '<a href="index.php?controller=Utilisateurs&action=show_login_page" id="logo"><img src="img/Agora_logo_final_CR_JulieValentin_redimensionne.png" alt="LogoAgora"> </a>';
 				}
 				?>
 				
