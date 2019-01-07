69ca2519a8601c504b1a3cbf500760178a4c1a83
Adrien Chaptal <adrien.chaptal@gmail.com>
2019-01-07 14:41:12 +0100

    ok

--- view/FaireExercice/correction.php
+++ view/FaireExercice/correction.php
@@ -1,16 +1,16 @@
-<?php
+
+            <form method="post" action="index.php?" id="formulaire_ajouterqcm">
+    
+            <fieldset>
+                <?php
     echo "<div>";
     echo "<h2> $nomE</h2>  <br/ >";
     echo "<p> Énoncé de l'exercice : $enonce </p>"; 
 
     echo "<p> Reponse de l'eleve $loginEtudiant : <br /> $reponse</p>";
 
-    echo "</div>";
+    echo '<input type="hidden" name="loginEtudiant" value="'. $loginEtudiant .'" /> <input type="hidden" name="id" value="'.$id.'" />';
 ?>
-
-            <form method="post" action="index.php?" id="formulaire_ajouterqcm">
-    
-            <fieldset>
             
             <p>
                 Note sur 20 : <br />
@@ -21,9 +21,8 @@
                 <label for="reponse_id">Commentaires:</label>
                 <textarea name="reponse" id="reponse_id" required></textarea>
                 
-                <input type="hidden" name="action" value="" />
+                <input type="hidden" name="action" value="noteExerciceClassique" />
                 <input type="hidden" name="controller" value="Notes" />
-                <input type="hidden" name="idExercice" value=<?=$id?> />
 
             </p>
             
