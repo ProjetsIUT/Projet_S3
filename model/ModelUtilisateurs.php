7fbb768244d13a849cde84be116de4857d0b6d1f
Julian Bourdes <julian.bourdes@gmail.com>
2019-01-10 21:55:16 +0100

    Tabulation modelUtilisateurs

--- model/ModelUtilisateurs.php
+++ model/ModelUtilisateurs.php
@@ -17,38 +17,38 @@ class ModelUtilisateurs extends Model {
 	  private $nonce;
 
     public function __construct($data = array()) {
-	  if (!empty($data)) {
-	    // If both $m, $c and $i are not NULL, 
-	    // then they must have been supplied
-		// so fall back to constructor with 3 arguments
-		if(isset($data["loginUtilisateur"])) {
-			$this->loginUtilisateur= $data["loginUtilisateur"];
-		}
+	  	if (!empty($data)) {
+			// If both $m, $c and $i are not NULL, 
+			// then they must have been supplied
+			// so fall back to constructor with 3 arguments
+			if(isset($data["loginUtilisateur"])) {
+				$this->loginUtilisateur= $data["loginUtilisateur"];
+			}
 
-		if(isset($data["prenomUtilisateur"];)) {
-			$this->prenomUtilisateur=$data["prenomUtilisateur"];
-		}
+			if(isset($data["prenomUtilisateur"];)) {
+				$this->prenomUtilisateur=$data["prenomUtilisateur"];
+			}
 
-		if(isset($data["nomUtilisateur"];)) {
-			$this->nomUtilisateur=$data["nomUtilisateur"];
-		}
+			if(isset($data["nomUtilisateur"];)) {
+				$this->nomUtilisateur=$data["nomUtilisateur"];
+			}
 
-		if(isset($data["mdpUtilisateur"];)) {
-			$this->mdpUtilisateur=$data["mdpUtilisateur"];
-		}
+			if(isset($data["mdpUtilisateur"];)) {
+				$this->mdpUtilisateur=$data["mdpUtilisateur"];
+			}
 
-		if(isset($data["emailUtilisateur"];)) {
-			$this->emailUtilisateur=$data["emailUtilisateur"];
-		}
+			if(isset($data["emailUtilisateur"];)) {
+				$this->emailUtilisateur=$data["emailUtilisateur"];
+			}
 
-		if(isset($data["codeEtablissement"];)) {
-			$this->codeEtablissement=$data["codeEtablissement"];
+			if(isset($data["codeEtablissement"];)) {
+				$this->codeEtablissement=$data["codeEtablissement"];
+			}
+			
+			if(isset($data["nonce"];)) {
+				$this->nonce=$data["nonce"];
+			}
 		}
-		
-		if(isset($data["nonce"];)) {
-			$this->nonce=$data["nonce"];
-		}
-	  	}
    	}
 
 	public function get($nom_attribut) {
