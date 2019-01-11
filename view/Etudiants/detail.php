227ba494a212de09c068c359c0d5bb21f35e373c
Julian Bourdes <julian.bourdes@gmail.com>
2019-01-11 23:16:44 +0100

    Modification vue supprimer étudiant

--- view/Etudiants/detail.php
+++ view/Etudiants/detail.php
@@ -11,7 +11,10 @@
     Moyenne générale actuelle : '.htmlspecialchars($umoyenneGenerale).'/20 <br> </p>';
     
 
-        echo ('<br> <a class= "bouton" href="index.php?controller=utilisateurs&action=read&loginUtilisateur=' . rawurlencode($ulogin) . '" > Retour sur les informations utilisateurs ! </a>');
+    if (Session::is_admin()) {
+        echo ('<br> <a class= "bouton_suppr" href="index.php?controller=etudiants&action=delete&loginEtudiant=' . rawurlencode($ulogin) . '" > Supprimer les informations universitaires de cet étudiant! </a>');
+    }
+        echo ('<br> <br> <a class= "bouton" href="index.php?controller=utilisateurs&action=read&loginUtilisateur=' . rawurlencode($ulogin) . '" > Retour sur les informations utilisateurs ! </a>');
         echo ('<br>  <br> <a class= "bouton" href="index.php?controller=etudiants&action=update&loginEtudiant=' . rawurlencode($ulogin) . '" > Modifier les informations universitaires ! </a>');
         
 ?>
