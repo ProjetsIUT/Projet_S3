d19f74324098e58cf2570f3fb8aefe23abca58ad
Julian Bourdes <julian.bourdes@gmail.com>
2019-01-11 10:43:53 +0100

    Gestion des erreurs générals

--- controller/ControllerUtilisateurs.php
+++ controller/ControllerUtilisateurs.php
@@ -81,29 +81,17 @@ class ControllerUtilisateurs extends Controller{
         }
     }
 
-    public static function show_home_page() {
+    public static function show_error_page() {
+        $error_code = 'Erreur';
         if(isset($_GET['error'])) {
             $error = $_GET['error'];
             if($error === '404') {
-                $error_page = 'Erreur 404 - Document Introuvable - vous avez été redirigé vers la page d\'accueil';
+                $error_code = 'Erreur 404 - Document Introuvable';
             }
         }
-        if(Session::is_student()){ //si c'est un étudiant 
-            $redirection = 'index.php?controller=etudiants&action=show_perso_page';
-            header('Refresh: 3; url='.$redirection);
-        }
-        else if(Session::is_teacher()){ //si c'est un enseignant
-            $redirection = 'index.php?controller=enseignants&action=show_perso_page';
-            header('Refresh: 3; url='.$redirection);
-        }
-        else if(Session::is_admin()){ //si c'est un admin
-            $redirection = 'index.php?controller=administrateur&action=show_perso_page';
-            header('Refresh: 3; url='.$redirection);
-        }
-        else {
-            $redirection = 'index.php';
-            header('Refresh: 3; url='.$redirection);
-        }
+        $pagetitle = 'Erreur';
+        require (File::build_path(array('view', 'error.php')));
+
     }
  
     public static function connect() {
