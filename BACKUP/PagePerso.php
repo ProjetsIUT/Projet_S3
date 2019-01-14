78b9a2e940a9d275df6e09be8fd670ef7c845c89
Adrien Chaptal <adrien.chaptal@gmail.com>
2019-01-14 08:17:49 +0100

    pre version finale

---
+++ BACKUP/PagePerso.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8" />
+		<title>Maquette</title>
+		 <link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+	
+		<header>
+				<div id="logo"><img src="img/LogoHomepage3.png" alt="LOGO FICTIF"> </div>
+
+				
+				<nav>
+
+						<div class="menu">
+							Cours
+						</div>
+						<div class="menu">
+							Exercices
+						</div>
+						<div class="menu">
+							Statistiques
+						</div>
+						
+						
+				</nav>
+
+				
+				<div id="connexion"><a id="connexion_inscription"><?php echo "Bonjour, $prenom"?></a></div>
+		</header>
+
+		<article id="page_connexion">
+
+
+			
+
+		</article>
+			
+
+	</body>
+</html>
